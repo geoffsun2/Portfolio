1c7f395bfa608a94a099005d24e932b660b44f8a
Geoff Sun <geoffsun2@gmail.com>
2023-10-27 15:45:18 -0400

    updated footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,11 @@
-<footer id='footer' class='py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center'>
+<footer id='footer' class='py-20 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center'>
   <p class='px-4 py-2 bg-white text-slate-950 font-medium rounded-lg'>Get in touch &darr;</p>
-  <div class='flex flex-col gap-4 items-center justify-center'>
+  <div class='flex flex-col gap-4 items-center justify-center text-base md:text-xl lg:text-2xl'>
     <p><b class='pr-2'>Email:</b> geoffsun2@gmail.com</p>
     <p><b class='pr-2'>GitHub:</b> 
-      <a class='text-violet-400' target='_blank' href='https://www.github.com/geoffsun2'>geoffsun2<sup><span class='text-xs scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]' /></span></sup></a></p>
+      <a class='text-violet-400' target='_blank' href='https://www.github.com/geoffsun2'>geoffsun2<sup><span class='scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square scale-[75%]' /></span></sup></a></p>
       <p><b class='pr-2'>LinkedIn:</b> 
-        <a class='text-violet-400' target='_blank' href='https://www.linkedin.com/in/geoffrey-sun'>Geoffrey-Sun<sup><span class='text-xs scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]' /></span></sup></a></p>
+        <a class='text-violet-400 ' target='_blank' href='https://www.linkedin.com/in/geoffrey-sun'>Geoffrey-Sun<sup><span class='scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square scale-[75%]' /></span></sup></a></p>
   </div>
 
 </footer>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -16,7 +16,7 @@
 
 <main class='flex flex-col flex-1 p-4'>
   <section id='introPage' class='grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14 relative'>
-    <div class='flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10 lg:pl-20'>
+    <div class='flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10 lg:pl-20 z-2'>
       <h2 class='font-semibold text-4xl sm:text-5xl md:text-6xl'>Hi! I'm <span class='poppins text-violet-400'>Geoff</span> Sun <br/> Full Stack <span class='poppins text-violet-400'>Developer</span> </h2>
       <p class='text-base sm:text-lg md:text-xl opacity-50'>
        (Pronounced Jeff)
@@ -24,13 +24,13 @@
       <p class='text-base sm:text-lg md:text-xl'>
         My <span class='text-violet-400'>favorite tech</span> includes JavaScript, React, Node.js + Express.js, Svelte, PostgreSQL & MongoDB!
       </p>
-      <a href='https://www.linkedin.com/in/geoffrey-sun' target='_blank' class='blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer'>
+      <!-- <a href='https://www.linkedin.com/in/geoffrey-sun' target='_blank' class='blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer'>
         <div class='absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200' />
         <h4 class='relative z-9'>Connect &rarr;</h4>
-      </a>
+      </a> -->
     </div>
     <div class='relative shadow-2xl grid place-items-center'>
-      <img src={'images/geoffnoBG.png'} alt='profile' class='object-cover z-[2] max-h-[70vh]' />
+      <img src={'images/geoffnoBG.png'} alt='profile' class='object-cover z-[-20] max-h-[70vh]' />
     </div>
 
   </section>
