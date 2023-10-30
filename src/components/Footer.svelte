8cb158ee36adeb2d7c99ae39bb724984b96f818e
Geoff Sun <geoffsun2@gmail.com>
2023-10-30 09:14:57 -0400

    added icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,21 @@
 <footer id='footer' class='py-20 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center'>
   <p class='px-4 py-2 bg-white text-slate-950 font-medium rounded-lg'>Get in touch &darr;</p>
   <div class='flex flex-col gap-4 items-center justify-center text-base md:text-xl lg:text-2xl'>
-    <p><b class='pr-2'>Email:</b> geoffsun2@gmail.com</p>
-    <p><b class='pr-2'>GitHub:</b> 
-      <a class='text-violet-400' target='_blank' href='https://www.github.com/geoffsun2'>geoffsun2<sup><span class='scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square scale-[75%]' /></span></sup></a></p>
+    <div class='flex items-center'>
+      <i class='fa-regular fa-envelope mr-2' />
+      <p><b class='pr-2'>Email:</b> geoffsun2@gmail.com</p>
+    </div>
+    <div class="flex items-center">
+      <img src='/images/githubIcon.png' alt='GitHub' class='h-6 mr-2'>
+      <p><b class='pr-2'>GitHub:</b> 
+        <a class='text-violet-400' target='_blank' href='https://www.github.com/geoffsun2'>geoffsun2<sup><span class='scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square scale-[75%]' /></span></sup></a></p>
+    </div>
+   <div class='flex items-center'>
+      <img src='/images/linkedInLogo.png' alt='linkedIn' class='h-6 mr-2'>
       <p><b class='pr-2'>LinkedIn:</b> 
-        <a class='text-violet-400 ' target='_blank' href='https://www.linkedin.com/in/geoffrey-sun'>Geoffrey-Sun<sup><span class='scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square scale-[75%]' /></span></sup></a></p>
+      <a class='text-violet-400 ' target='_blank' href='https://www.linkedin.com/in/geoffrey-sun'>Geoffrey-Sun<sup><span class='scale-75 pl-0.5'><i class='fa-solid fa-arrow-up-right-from-square scale-[75%]' /></span></sup></a></p>
+   </div>
+     
   </div>
 
 </footer>
