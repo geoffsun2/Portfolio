c026afa81b7e4ea2cebbe968d87f9ecce36d698f
Geoff Sun <geoffsun2@gmail.com>
2023-11-16 10:05:14 -0500

    added web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
   import Header from '../components/Header.svelte';
   import Footer from '../components/Footer.svelte'
 
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+ 
+  inject({ mode: dev ? 'development' : 'production' });
+
   let y;
   let innerHeight = 0;
   let innerWidth = 0;
